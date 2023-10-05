73df9d3df82c5b580db6b7363c47911f15739f21
Drophang <victor1998kombad@gmail.com>
2023-10-05 11:06:10 -0400

    Cambio

---
+++ src/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        Persona Per = new Persona("Nelson");
+    }
+}
---
+++ src/Persona.java
@@ -0,0 +1,6 @@
+public class Persona {
+    private String Nombre;
+    public Persona (String Nombre){
+        this.Nombre = Nombre;
+    }
+}
